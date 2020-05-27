403a026d6addb4594c4eadbefefd2608c33c8777
viky <viky_zambi95@hotmail.com>
2020-05-27 09:39:45 -0300

    subo ejericio 9 pero tiene errores

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -1,11 +1,8 @@
 (ns exercise9)
 
-(defn word-count 
+(defn word-count
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (throw (Exception. "Not implemented."))
+  (def a [(apply str (remove #((set ",:!!&@$%^&") %) (clojure.string/lower-case x)))])
+  (frequencies a)
 )
-
-
-
-
