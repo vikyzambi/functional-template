15342790b13bc413eb7e3dd8a09753416cc817df
viky <viky_zambi95@hotmail.com>
2020-05-27 20:14:42 -0300

    subo ejercicio 9 funciona OK

--- src/exercise9.clj
+++ src/exercise9.clj
@@ -3,6 +3,7 @@
 (defn word-count
   "Returns a map where each key is a distinct word in a given sentence and each value is the number of occurences of that word in the sentence."
   [x]
-  (def a [(apply str (remove #((set ",:!!&@$%^&") %) (clojure.string/lower-case x)))])
-  (frequencies a)
+
+  (def a (apply str (remove #((set ",:!!&@$%^&") %) (clojure.string/lower-case x))))
+  (frequencies (clojure.string/split a #"\s+"))
 )
